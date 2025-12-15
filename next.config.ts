8d19db0c04ea0461e27233bb28292dcbdfa41927
Javier <jav.teys@gmail.com>
2025-12-15 16:13:36 +0800

    1

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Static export
+  //output: 'export', // Static export
   images: {
     unoptimized: true,
   },
